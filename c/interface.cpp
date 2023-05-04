96b25cbc7409d5d892990d052b3951be23984d62
pablo <pabloamartinez@unc.edu.ar>
2023-05-04 10:29:42 -0300

    add examples to C

---
+++ c/interface.cpp
@@ -0,0 +1,17 @@
+//name: reciprocal.cpp
+
+double reciprocal(int i)
+{
+   my_win.my_reciprobal();
+}
+
+
+
+
+
+ARCH=linux
+
+if ARCH==linux:
+	linux/src/reciprocal.cpp
+else:
+	win/src/reciprocal.cpp
---
+++ c/str/str_upper.c
@@ -0,0 +1,14 @@
+#include <stdio.h>  
+#include <string.h>   
+ 
+/*
+* C strupr() function converts a string to uppercase letters.
+*
+* DEPRECATED MS WINDOWS FUNCTION
+*
+*/
+void main()
+{    
+	char str[10] = "Hello C.";  
+	printf("%s",strupr(str));    
+}
